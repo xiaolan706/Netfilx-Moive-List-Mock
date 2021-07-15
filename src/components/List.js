b841f4dd8802197fcfb1cbe425fde529a2e82fa7
Jin (Sophia) Tang <42655902+xiaolan706@users.noreply.github.com>
2021-07-14 17:48:34 -0700

    Add files via upload

--- src/components/List.js
+++ src/components/List.js
@@ -1,17 +1,32 @@
-import react from 'react';
-import Movie from './Movie';
+import react, {useState} from 'react';
+// import Movie from './Movie';
 import styled from "styled-components";
 
 const List = ({className, section, data, buttonName, toggleButton}) => {
+    const [showButton, setShowButton] = useState(false);
+
     return (
         <div>
             <h2 className="section">{section}</h2>
             <div className={className}>
                 {data.map((movie) => {
                     return (
-                        <Movie key={movie.id} img={movie.img} title={movie.title}>
-                            <button onClick={() => toggleButton(movie)}>{buttonName}</button>
-                        </Movie>    
+                        <div className='movieCardContainer' key={movie.id}
+                            onMouseEnter={() => setShowButton(true)}
+                            onMouseLeave={() => setShowButton(false)}
+                        >
+                            <img className="imageContainer" src={movie.img} alt={movie.title}/>
+                            <div className="titleContainer">
+                                <p className="title">{movie.title}</p>
+                                {showButton && <button className="button" onClick={() => toggleButton(movie)}>{buttonName}</button>}
+                            </div>    
+                        </div>
+
+
+
+                        // <Movie key={movie.id} img={movie.img} title={movie.title}>
+                        //     <button onClick={() => toggleButton(movie)}>{buttonName}</button>
+                        // </Movie>    
                     )
                 })}
             </div>
@@ -24,7 +39,22 @@ export default styled(List)`
     grid-template-columns: repeat(4, 1fr);
     grid-gap: 1rem;
 
+    .movieCardContainer{
+        display: flex;
+        justify-self: center;
+        flex-direction: column;
+        min-width:300px;
+    }
+    
     .section {
         padding-left: 50px;
+    }
+    
+    .titleContainer {
+        text-align: center;
+    }
+
+    .title {
+        padding: 0 20px;;
     }
 `;
