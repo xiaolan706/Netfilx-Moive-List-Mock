931411413389edad8b7a9721515d7482c8c247f0
Jin (Sophia) Tang <42655902+xiaolan706@users.noreply.github.com>
2021-07-14 21:36:58 -0700

    Add files via upload

--- src/components/List.js
+++ src/components/List.js
@@ -1,32 +1,25 @@
-import react, {useState} from 'react';
-// import Movie from './Movie';
+import react, { useState } from 'react';
 import styled from "styled-components";
 
 const List = ({className, section, data, buttonName, toggleButton}) => {
-    const [showButton, setShowButton] = useState(false);
+    const [showButton, setShowButton] = useState();
 
     return (
-        <div>
-            <h2 className="section">{section}</h2>
-            <div className={className}>
+        <div className={className} >
+            <h2>{section}</h2>
+            <div className="listContainer" >
                 {data.map((movie) => {
                     return (
                         <div className='movieCardContainer' key={movie.id}
-                            onMouseEnter={() => setShowButton(true)}
-                            onMouseLeave={() => setShowButton(false)}
+                            onMouseEnter={() => setShowButton(movie.id)}
+                            onMouseLeave={() => setShowButton(-1)}
                         >
-                            <img className="imageContainer" src={movie.img} alt={movie.title}/>
+                            <img src={movie.img} alt={movie.title}/>
                             <div className="titleContainer">
                                 <p className="title">{movie.title}</p>
-                                {showButton && <button className="button" onClick={() => toggleButton(movie)}>{buttonName}</button>}
+                                {showButton == movie.id && <button className="button" onClick={() => toggleButton(movie)}>{buttonName}</button>}
                             </div>    
-                        </div>
-
-
-
-                        // <Movie key={movie.id} img={movie.img} title={movie.title}>
-                        //     <button onClick={() => toggleButton(movie)}>{buttonName}</button>
-                        // </Movie>    
+                        </div> 
                     )
                 })}
             </div>
@@ -35,9 +28,12 @@ const List = ({className, section, data, buttonName, toggleButton}) => {
 }
 
 export default styled(List)`   
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    grid-gap: 1rem;
+    padding-left: 50px;
+
+    .listContainer {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        grid-gap: 1rem;
 
     .movieCardContainer{
         display: flex;
@@ -46,15 +42,12 @@ export default styled(List)`
         min-width:300px;
     }
     
-    .section {
-        padding-left: 50px;
-    }
-    
-    .titleContainer {
-        text-align: center;
+    .title {
+        padding: 0 20px 0 100px;
     }
 
-    .title {
-        padding: 0 20px;;
+    .button {
+        height:50px;
+        width:80px;
     }
 `;
